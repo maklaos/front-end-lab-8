058d45f9474553ecec5aa492198425cfe200c131
maklaos <maklaosdelay@gmail.com>
2018-02-08 23:37:12 +0200

    done

---
+++ homework_07/js/task1.js
@@ -0,0 +1,25 @@
+let N = 0;
+
+while (N <= 0 || N > 20 || !Number.isInteger(N)) {
+  N = +prompt('Enter natural number between 1 and 20');
+}
+
+let space = '   ';
+let block = '[~]';
+
+let result = '';
+
+for (let i = 1; i <= N; i++) {
+
+  for (let j = 0; j < N-i; j++) {
+    result += space;
+  }
+
+  for (let k = 0; k < i*2-1; k++) {
+    result += block;
+  }
+
+  result += '\n';
+}
+
+console.log(result);
---
+++ homework_07/js/task2.js
@@ -0,0 +1,40 @@
+let start = confirm('Do you want to play a game?');
+
+if (start) {
+  let gameCount = 1;
+  let isPlaying = true;
+  let prize = 0;
+  let prizes = [2, 5, 10];
+
+  while (isPlaying) {
+    let numberToGuess = Math.round(Math.random() * 5 * gameCount);
+    let range = 5 * Math.pow(2, gameCount-1);
+    let attempts = 3;
+    let userInput = -1;
+
+    while (userInput != numberToGuess && attempts != 0) {
+      currentAttemptPrize = prizes[attempts -1] * Math.pow(3, gameCount-1);
+
+      userInput = prompt('Enter a number from 0 to ' + range +
+       '\nAttempts left: ' + attempts +
+        '\nTotal prize: ' + prize  + '$' +
+        '\nPossible price on current attempt: ' + currentAttemptPrize + '$');
+
+      attempts--;
+    }
+
+    if (userInput == numberToGuess) {
+      prize += currentAttemptPrize;
+    } else {
+      break;
+    }
+
+    isPlaying = confirm('Are you whant to continue a game?');
+
+    gameCount++;
+  }
+
+  console.log('Thank you for a game. Your prize is: ' + prize + '$');
+} else {
+  console.log('You did not become a millionaire');
+}
