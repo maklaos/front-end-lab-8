de300bc04d45092b7d1dfde2b2b8a7e759e1e51d
maklaos <maklaosdelay@gmail.com>
2018-02-15 20:34:30 +0200

    less symbols in one line

--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -1,3 +1,4 @@
 function reverseNumber(val) {
-  return Number(Math.abs(val).toString().split("").reverse().join("")) * Math.abs(val) / val;
+  let reversed = Math.abs(val).toString().split("").reverse().join("");
+  return Number(reversed) * Math.abs(val) / val;
 }
