7d8ef32c605d78b0f260e8660892e1a4c1c4bbf2
maklaos <maklaosdelay@gmail.com>
2018-02-16 20:17:41 +0200

    test+

--- homework_09/js/decypherPhrase.js
+++ homework_09/js/decypherPhrase.js
@@ -5,4 +5,4 @@ function decypherPhrase(obj, str) {
 //test
 console.log('\ndecypherPhrase');
 var charactersMap = {a: 'o', c: 'd', t: 'g'}
-console.log(decypherPhrase(charactersMap, 'kiggy dog'));
+console.log(decypherPhrase(charactersMap, 'mewkiggy dog'));
