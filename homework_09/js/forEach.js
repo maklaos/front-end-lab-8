45705ead0b3fe3277c5d4c6b77bf2db3a2d4709d
maklaos <maklaosdelay@gmail.com>
2018-02-16 20:17:37 +0200

    chenge foreach to for

--- homework_09/js/forEach.js
+++ homework_09/js/forEach.js
@@ -1,9 +1,11 @@
 function forEach(arr, func) {
-  arr.forEach(func);
+  for (let i = 0; i < arr.length; i++) {
+    func(arr[i]);
+  }
 }
 
 //test
 console.log('forEach:');
 forEach([3, 5, 2], function(el) {
-console.log(el);
+  console.log(el);
 });
