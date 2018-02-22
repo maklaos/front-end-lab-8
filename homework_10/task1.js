c6c55987580a5212f7c09682a37790e1fa69bb6f
maklaos <maklaosdelay@gmail.com>
2018-02-22 22:57:19 +0200

    done

---
+++ homework_10/task1.js
@@ -0,0 +1,37 @@
+function debounce(callback, delay) {
+  let timeout = false;
+
+  return function() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(callback, delay);
+  }
+}
+
+// Example
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 1'
